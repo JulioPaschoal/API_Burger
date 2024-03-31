9efdefc2d3bd25e35306fd963d999387ca70466d
JULIOPASCHOAL <juliocpaschoal@gmail.com>
2024-03-31 11:24:25 -0300

    corigindo bug na validação de sessions

---
+++ src/app/controller/SessionController.js
@@ -0,0 +1,42 @@
+// CONF. MODULES \\
+import * as Yup from 'yup';
+import User from '../models/User';
+
+// CONF. SESSIONS \\
+class SessionController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      email: Yup.string().email().required(),
+      password: Yup.string().required(),
+    });
+
+    // VERIFICANDO EMAIL E SENHA \\
+    if (!(await schema.isValid(req.body))) {
+      return res
+        .status(401)
+        .json({ error: 'Make sure your password or email are correct' });
+    }
+
+    // VERIFICANDO SE EXISTE USUARIO \\
+    const { email, password } = req.body;
+    const user = await User.findOne({
+      where: { email },
+    });
+    if (!user) {
+      return res
+        .status(401)
+        .json({ error: 'Make sure your password or email are correct' });
+    }
+
+    // VERIFICACAO DE SENHA NO BANDO \\
+    if (!(await user.checkPassword(password))) {
+      return res
+        .status(401)
+        .json({ error: 'Make sure your password or email are correct' });
+    }
+
+    return res.json({ id: user.id, email: user.name, admin: user.admin });
+  }
+}
+
+export default new SessionController();
