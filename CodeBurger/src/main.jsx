d0aac78d8017fce0156637d8793dae5ae886dac8
JULIOPASCHOAL <juliocpaschoal@gmail.com>
2024-04-15 08:07:32 -0300

    Refatorando Projeto procompleto

--- CodeBurger/src/main.jsx
+++ CodeBurger/src/main.jsx
@@ -1,12 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { Login } from './pages/login'
-import GlobalStyles from './styles/globalStyles'
-
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Login from './pages/login';
+import GlobalStyles from './styles/globalStyles';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <GlobalStyles />
-    <Login />
+    <GlobalStyles/>
+    <Login/>
   </React.StrictMode>,
-)
+);
--- CodeBurger/src/pages/login/index.jsx
+++ CodeBurger/src/pages/login/index.jsx
@@ -1,7 +1,22 @@
-export function Login(){
-    return(
-        <>
-          <h1>Tela De login</h1>
-        </>
-    )
+import {Container, Background, ContainerItens, Label, Input, Button, SignInLink  } from './styles';
+export default function Login() {
+  return (
+    <>
+      <Container>
+        <Background />
+        <ContainerItens>
+          <img />
+          <h1>Login</h1>
+          <Label>Email</Label>
+          <Input />
+          <Label>Senha</Label>
+          <Input />
+          <Button>Sign In</Button>
+        </ContainerItens>
+        <SignInLink>
+          Não possui conta? <a>Sign Up</a>
+        </SignInLink>
+      </Container>
+    </>
+  )
 }
